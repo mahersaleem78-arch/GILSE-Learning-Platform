87fb768f661662052cadd432f66f3362a7c97c0e
mahersaleem78-arch <mahersaleem78@gmail.com>
2026-09-02 14:58:01 +0300

    Add instructor route

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import ReferralLandingPage from './pages/ReferralLandingPage'
 import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignupPage'
 import DashboardPage from './pages/DashboardPage'
+import InstructorCoursesPage from './pages/InstructorCoursesPage'
 import AdminPage from './pages/AdminPage'
 import AdminCoursesPage from './pages/admin/AdminCoursesPage'
 import AdminCourseDetailPage from './pages/admin/AdminCourseDetailPage'
@@ -33,6 +34,7 @@ export default function App() {
     <Route element={<AuthLayout />}><Route path="/login" element={<LoginPage />} /><Route path="/signup" element={<SignupPage />} /></Route>
     <Route path="/dashboard" element={<ProtectedRoute><StudentLayout /></ProtectedRoute>}><Route index element={<DashboardPage />} /></Route>
     <Route path="/courses/:id/pay" element={<ProtectedRoute><PaymentPage /></ProtectedRoute>} />
+    <Route path="/instructor/courses" element={<RoleProtectedRoute roles={['instructor']}><StudentLayout /></RoleProtectedRoute>}><Route index element={<InstructorCoursesPage />} /></Route>
     <Route path="/admin" element={<RoleProtectedRoute roles={['admin','developer']}><AdminLayout /></RoleProtectedRoute>}>
       <Route index element={<AdminPage />} /><Route path="courses" element={<AdminCoursesPage />} /><Route path="courses/:id" element={<AdminCourseDetailPage />} /><Route path="payments" element={<AdminPaymentsPage />} /><Route path="rewards" element={<AdminRewardsPage />} />
     </Route>
