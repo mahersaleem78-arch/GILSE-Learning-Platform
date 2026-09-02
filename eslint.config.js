9dc7bab59aa53c82853d973eb33c89a32851b846
mahersaleem78-arch <mahersaleem78@gmail.com>
2026-09-02 14:31:51 +0300

    Fix ESLint flat config to use existing locked dependencies

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,27 @@
-import js from '@eslint/js'
-import tseslint from 'typescript-eslint'
+import tsParser from '@typescript-eslint/parser'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import globals from 'globals'
 
-export default tseslint.config(
+export default [
   { ignores: ['dist', 'node_modules', 'supabase/functions'] },
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
     languageOptions: {
-      globals: { ...globals.browser, ...globals.node },
-      parserOptions: { ecmaFeatures: { jsx: true } },
+      parser: tsParser,
+      parserOptions: { ecmaVersion: 'latest', sourceType: 'module', ecmaFeatures: { jsx: true } },
+      globals: {
+        window: 'readonly', document: 'readonly', navigator: 'readonly', localStorage: 'readonly',
+        console: 'readonly', fetch: 'readonly', URL: 'readonly', setTimeout: 'readonly', clearTimeout: 'readonly',
+      },
     },
     plugins: {
+      '@typescript-eslint': tsPlugin,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
     },
     rules: {
+      ...tsPlugin.configs.recommended.rules,
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
       'react-hooks/rules-of-hooks': 'error',
@@ -25,4 +29,4 @@ export default tseslint.config(
       'react-refresh/only-export-components': 'warn',
     },
   },
-)
+]
