9cbcbe989a2c7a3ca45f3429477333419dba5db4
mahersaleem78-arch <mahersaleem78@gmail.com>
2026-09-02 14:35:40 +0300

    Remove non-actionable React refresh lint warning

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 import tsParser from '@typescript-eslint/parser'
 import tsPlugin from '@typescript-eslint/eslint-plugin'
 import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
   { ignores: ['dist', 'node_modules', 'supabase/functions'] },
@@ -18,7 +17,6 @@ export default [
     plugins: {
       '@typescript-eslint': tsPlugin,
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
     },
     rules: {
       ...tsPlugin.configs.recommended.rules,
@@ -26,7 +24,6 @@ export default [
       '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'off',
-      'react-refresh/only-export-components': 'warn',
     },
   },
 ]
